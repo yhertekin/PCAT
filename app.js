6cb4c6c16c4b7cb07bf2343f56c03a0432675258
yhertekin <yhertekin@gmail.com>
2021-11-07 14:05:40 +0300

    add mongoose and db make add photo func

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require("express");
-const ejs = require("ejs");
-const path = require("path");
+const mongoose = require("mongoose");
+
+const Photo = require("./models/Photo");
+
+// const ejs = require("ejs");
+// const path = require("path");
 
 const app = express();
+// connect database
+mongoose.connect("mongodb://localhost/pcat-test-db");
 
 app.set("view engine", "ejs");
 
+//middlewares
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.render("index");
+app.get("/", async (req, res) => {
+	const photos = await Photo.find({});
+	res.render("index", { photos });
 });
 
 app.get("/add", (req, res) => {
@@ -18,6 +28,11 @@ app.get("/add", (req, res) => {
 
 app.get("/about", (req, res) => {
 	res.render("about");
+});
+
+app.post("/photos", async (req, res) => {
+	await Photo.create(req.body);
+	res.redirect("/");
 });
 
 const port = 3000;
